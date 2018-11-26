986a5539e3be9ac7ca0f22c66c30d9d22ebeacc5
Ofer Ben-Yacov <ofer.benyacov@toganetworks.com>
2018-11-26 08:40:02 +0200

    Kalman filter

--- pomdp/chatbot/slack_connector.py
+++ pomdp/chatbot/slack_connector.py
@@ -14,9 +14,9 @@ from rasa_core.training import online
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 
-TOKEN = 'xoxp-331933815733-331258863217-466127392353-de9b34da0f123c8d7e03d1e3d5264062'
-BOT_TOKEN = 'xoxb-331933815733-467511538663-SaCGwmq9hGiOW713QPHuisoU'
-VERIFICATION_TOKEN = 'Z0qtCWe8FPf8NLeuH12UiqZz'
+TOKEN = 'xoxp-'
+BOT_TOKEN = 'xoxb-'
+VERIFICATION_TOKEN = 'Z0q'
 
 inter = RasaNLUInterpreter("./models/current/nlu", "../data/nlu_config.yml")
 agent = Agent.load('models/dialogue', interpreter=inter,
@@ -25,7 +25,7 @@ agent = Agent.load('models/dialogue', interpreter=inter,
 if __name__ == '__main__':
     slack_input = SlackInput.from_credentials({
         'slack_token': BOT_TOKEN,
-        'slack_channel': 'DDQDG62BU'
+        'slack_channel': 'DD'
     })
     logger.debug('starting channel....')
     agent.handle_channels([slack_input], 5051)
