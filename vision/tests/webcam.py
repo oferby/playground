f803ba74cf3da38709c48496e43ff9443d0c2cc9
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2018-12-10 15:52:51 +0200

    vision

---
+++ vision/tests/webcam.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pygame
+import pygame.camera
+import matplotlib.pyplot as plt
+
+
+pygame.camera.init()
+pygame.camera.list_cameras()
+cam = pygame.camera.Camera('/dev/video0', (640,480))
+cam.start()
+
+img = cam.get_image()
+
+plt.imshow(img)
+
