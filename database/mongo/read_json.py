ecbb57cc89cf400ffac107396d857b85a68bbf9a
Ofer Ben-Yacov <ofer.benyacov@gmail.com>
2018-12-10 15:50:44 +0200

    add db support

--- database/mongo/read_json.py
+++ database/mongo/read_json.py
@@ -1,7 +1,7 @@
 import json
 from pprint import pprint
 
-with open('/home/stack/PycharmProjects/playground/pomdp/chatbot/db/data.txt') as f:
+with open('/home/stack/PycharmProjects/playground/pomdp/chatbot/db/data.json') as f:
     data = json.load(f)
 
 pprint(data)
