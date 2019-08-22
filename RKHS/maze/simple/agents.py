bede052fcebf2bf626593dbb0a764bef62dd409d
Ofer Ben-Yacov <ofer.benyacov@toganetworks.com>
2019-08-22 16:23:59 +0300

    update move

--- RKHS/maze/simple/agents.py
+++ RKHS/maze/simple/agents.py
@@ -61,24 +61,56 @@ class BayesAgent(Agent):
         self.Zm = np.matrix(world.Z)
         self.observations = len(world.Z)
         self.prior = world.prior
+        self.prior_dim = int(np.sqrt(len(self.prior)))
 
         self.obstacles = world.obstacles
 
     def flatten(self, a):
-        return np.squeeze(np.asarray(a))
+        return np.asarray(a).reshape(-1)
 
     def get_action(self, obs):
         action = kb_action()
         return action
 
+    def normalize(self, arr):
+        s = arr.sum()
+        if s == 0:
+            return arr
+        return arr / s
+
+    def update_move(self, action):
+        dim = self.prior_dim
+        prior = self.prior.reshape(dim, dim)
+        zz = np.matrix(np.zeros(dim))
+        if action == 0:
+            p_ = prior[1:, :]
+            prior = np.vstack([p_, zz])
+        elif action == 1:
+            p_ = prior[:, 0:-1]
+            prior = np.hstack([zz.transpose(), p_])
+        elif action == 2:
+            p_ = prior[0:-1, :]
+            prior = np.vstack([zz, p_])
+        else:
+            p_ = prior[:, 1:]
+            prior = np.hstack([p_, zz.transpose()])
+
+        prior = self.normalize(prior)
+        self.prior = self.flatten(prior)
+
+    def update_observation(self):
+        pass
+
     def get_observation(self, obs, action, reward, done):
         super().get_observation(obs, action, reward, done)
 
+        self.update_move(action)
+
         pZ = self.get_obs_vector(int(obs))
         posterior = self.flatten(pZ * self.Zm)  # * self.flatten(self.prior)
-        self.prior = posterior
-        print('real: {}'.format(self.obstacles[0]))
-        print('state: {}'.format(posterior[0:10]))
+        # self.prior = posterior
+        # print('real: {}'.format(self.obstacles[0]))
+        print('state: {}'.format(self.prior[0:10]))
 
     def get_obs_vector(self, obs):
         z = np.zeros(self.observations)
