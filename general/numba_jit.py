2f342b1ac268dd9d0148e183486b01cc84496107
oferb <ofer.benyacov@gmail.com>
2021-04-29 09:11:56 +0300

    numba JIT

--- general/numba_jit.py
+++ general/numba_jit.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import numba
 import random
 import time
@@ -26,28 +25,11 @@ def monte_carlo_pi_no_numba(samples: int):
 
 
 t1 = time.time_ns()
-print(monte_carlo_pi(10000000))
+print("pi: ", monte_carlo_pi(10000000))
 t2 = time.time_ns()
-print("time: ", t2 - t1)
+print("time without numba: ", t2 - t1)
 
 t1 = time.time_ns()
-print(monte_carlo_pi_no_numba(10000000))
+print("pi: ", monte_carlo_pi(10000000))
 t2 = time.time_ns()
-print("time: ", t2 - t1)
-
-
-=======
-# https://neurohackademy.github.io/high-performance-python/05-numba/
-from numba import jit
-
-@jit
-def fib(n):
-    a, b = 1, 1
-    for i in range(n):
-        a, b = a+b, a
-    return a
-
-
-for x in range(10,20,1):
-    print(fib(x))
->>>>>>> ab4510f8e2b705dbe6d130307cc165d6ecbae3af
+print("time with numba:", t2 - t1)
